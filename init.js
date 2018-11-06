86f9d2662badd1c53c0d1650f11e46d78774b9ca
qichangjun <1061427304@qq.com>
2018-11-06 10:58:36 +0800

    添加工具集

---
+++ init.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node  
+'use strict'
+
+var exec = require('child_process').exec;
+var operation = require('./lib/dist_operation')
+const program = require('commander');
+var fs=require('fs');
+var stat=fs.stat;
+
+program.on('--help', function () {
+    console.log('  Examples:')
+    console.log('')
+    console.log('    name +你当前路径所要转移的文件夹')    
+    console.log('')
+    })
+program
+    .command('name [name]')
+    .description('移动文件夹到web-serve服务')
+    .action(function(name){        
+        // 删掉dist文件夹
+        console.log('正在移除dist文件夹....')
+        operation.deleteFolder(__dirname+'/dist');
+        // 重新创建dist,移动目标文件夹
+        console.log('正在重新创建dist,移动目标文件夹....')
+        function  execute(){
+            var cmd=`cd ${__dirname} && npm run start:dev`;
+            console.log('正在启动服务....')
+            exec(cmd, function(error, stdout, stderr) {                    
+                if(error){
+                    console.log(error);
+                }
+                else{
+                    console.log("成功");
+                }
+            });
+        }
+        operation.createWebProject(execute)
+    })
+
+program.parse(process.argv);
+
